04c04ceb1a58f528932290707bd619a1243ad890
Rails Udemy Course <railsudemycourse@gmail.com>
2016-03-15 04:16:00 +0000

    Add admin user functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -59,7 +59,7 @@ class ArticlesController < ApplicationController
     
     def require_same_user
       # set restriction for editing articles only to the one created them
-      if current_user != @article.user
+      if current_user != @article.user && !current_user.admin?
         flash[:danger] = "You can only edit or delete your own articles"
         redirect_to root_path
       end
